fe8478fb2835acd196887260fd8b967e0c17925f
Kushal Ghimire <kushal.upr@gmail.com>
2026-08-19 12:03:30 +0545

    fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,7 @@ import EmployerApplicationsPage from "./pages/EmployerApplicationsPage";
 import NotificationsPage from "./pages/NotificationsPage";
 import InterviewsPage from "./pages/InterviewsPage";
 import ProtectedRoute from "./components/ProtectedRoute";
-// Define your routes hereeeeeeee
+// Define your routes hereeee
 
 export default function App() {
   return <Routes>
