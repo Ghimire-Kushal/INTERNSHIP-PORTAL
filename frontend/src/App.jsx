5bd036fc8636652c721a407e3a49d6e1f10ce4e0
Kushal Ghimire <kushal.upr@gmail.com>
2026-08-20 13:09:39 +0545

    push

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,7 @@ import EmployerApplicationsPage from "./pages/EmployerApplicationsPage";
 import NotificationsPage from "./pages/NotificationsPage";
 import InterviewsPage from "./pages/InterviewsPage";
 import ProtectedRoute from "./components/ProtectedRoute";
-
+//
 
 export default function App() {
   return <Routes>
